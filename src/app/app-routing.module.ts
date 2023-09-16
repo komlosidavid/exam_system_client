f86011dcb7537ace7221b7f29bfecc2a8e7096e3
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-16 17:52:57 +0200

    Added test route and dashboard route to the routeing system.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test/:id',
+    loadChildren: () =>
+      import('./test/test.module').then((module) => module.TestModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((module) => module.DashboardModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
