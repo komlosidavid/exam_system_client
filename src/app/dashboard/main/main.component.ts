809c3ee187837759b7b1f0461c3b42c795e8501f
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-16 21:27:01 +0200

    Update main.component.ts
    Create implementation with the OnInit interface and implemented ngOnInit method to get test datas.

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
+import { Test } from 'src/app/interfaces/testInterface';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  tests!: Array<Test>;
 
+  constructor(private service: DashboardService) {}
+
+  ngOnInit(): void {
+    this.service.getAllTests().subscribe({
+      next: (response) => {
+        this.tests = response;
+      },
+    });
+  }
 }
