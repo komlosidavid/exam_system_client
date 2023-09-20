ec901d06f12e0e4be67d28d06b4a55791f7df831
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:27:32 +0200

    Update test.component.ts

--- src/app/test/test/test.component.ts
+++ src/app/test/test/test.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Test } from 'src/app/interfaces/testInterface';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
+  test!: Test;
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.test = JSON.parse(params['data']);
+      console.log(this.test);
+    });
+  }
 }
