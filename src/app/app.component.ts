eda25ccc54f25e409974b585006f1eddd883630f
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:26:19 +0200

    Update app.component.ts
    This code will be reimplemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'exam.system.client';
+export class AppComponent implements OnInit {
+  accessToken: string | null = localStorage.getItem('accessToken');
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (!this.accessToken) {
+      this.router.navigateByUrl('auth');
+    } else {
+      this.router.navigateByUrl('dashboard');
+    }
+  }
 }
