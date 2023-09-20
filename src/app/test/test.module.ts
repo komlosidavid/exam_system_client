5eb4bf46589d11cc4abe23c3513af3b6d473d2ef
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:27:40 +0200

    Update test.module.ts

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -4,22 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TestRoutingModule } from './test-routing.module';
 import { QuestionComponent } from './question/question.component';
 
-import {
-  ClrRadioModule,
-  ClrCheckboxModule,
-  ClrTextareaModule,
-} from '@clr/angular';
 import { AnswerComponent } from './answer/answer.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [QuestionComponent, AnswerComponent, TestComponent],
-  imports: [
-    CommonModule,
-    TestRoutingModule,
-    ClrRadioModule,
-    ClrCheckboxModule,
-    ClrTextareaModule,
-  ],
+  imports: [CommonModule, TestRoutingModule],
 })
 export class TestModule {}
