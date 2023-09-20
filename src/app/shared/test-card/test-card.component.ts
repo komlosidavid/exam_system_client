0e1583ab688db6158c0a2b93ca20c16412d43842
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:27:50 +0200

    Update test-card.component.ts

--- src/app/shared/test-card/test-card.component.ts
+++ src/app/shared/test-card/test-card.component.ts
@@ -1,37 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { Test } from 'src/app/interfaces/testInterface';
+import { User } from 'src/app/interfaces/userInterface';
 
 @Component({
   selector: 'app-test-card',
   templateUrl: './test-card.component.html',
   styleUrls: ['./test-card.component.css'],
 })
-export class TestCardComponent {
-  submenu_icon: string = 'fa-chevron-down';
+export class TestCardComponent implements OnInit {
   is_submenu_open: boolean = false;
   dropdown_open: boolean = false;
+
+  menu_items: MenuItem[] | undefined;
 
   @Input()
   test!: Test;
 
-  openCollaboratorsSubmenu(): void {
-    this.toggleSubmenuIcon();
-    this.toggleSubmenu();
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.menu_items = [
+      {
+        label: 'Settings',
+        items: [
+          {
+            label: 'Edit test',
+            icon: 'pi pi-file-edit',
+          },
+        ],
+      },
+    ];
   }
 
-  private toggleSubmenuIcon(): void {
-    if (this.submenu_icon == 'fa-chevron-down') {
-      this.submenu_icon = 'fa-chevron-up';
-    } else {
-      this.submenu_icon = 'fa-chevron-down';
-    }
-  }
-
-  private toggleSubmenu(): void {
+  toggleCollaboratorsSubmenu(): void {
     this.is_submenu_open = !this.is_submenu_open;
   }
 
-  toggleSettingsDropdown(): void {
-    this.dropdown_open = !this.dropdown_open;
+  getCollaboratorInitials(collaborator: User): string {
+    var names = collaborator.fullname.split(' ');
+    return names[0].charAt(0) + names[1].charAt(0);
+  }
+
+  onHandleNavigateToTest(test: Test): void {
+    this.router.navigate(['/test', { data: JSON.stringify(test) }]);
   }
 }
