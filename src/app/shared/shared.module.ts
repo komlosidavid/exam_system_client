035647a804e9841dc8a4883f14ca9e247f7cddd2
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:27:55 +0200

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,29 @@ import { NavComponent } from './nav/nav.component';
 
 import { ButtonComponent } from './button/button.component';
 import { TestCardComponent } from './test-card/test-card.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { MenuModule } from 'primeng/menu';
+import { TooltipModule } from 'primeng/tooltip';
+import { CardModule } from 'primeng/card';
+import { AvatarModule } from 'primeng/avatar';
+import { AnimateModule } from 'primeng/animate';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @NgModule({
   declarations: [NavComponent, ButtonComponent, TestCardComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    PanelModule,
+    MenuModule,
+    TooltipModule,
+    CardModule,
+    AvatarModule,
+    AnimateModule,
+    ProgressBarModule,
+  ],
   exports: [NavComponent, ButtonComponent, TestCardComponent],
 })
 export class SharedModule {}
