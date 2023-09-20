81b5a4b96c06ffcfdcc9895bf8193f480901364b
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 22:54:59 +0200

    Update nav.component.ts

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  badgeValue: number = 0;
+  isAuthenticated: boolean = false;
 
+  constructor(private router: Router, private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    this.sharedService.isAuthenticated$.subscribe((status) => {
+      this.isAuthenticated = status;
+    });
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.sharedService.setAuthenticationStatus(false);
+    this.router.navigateByUrl('/auth');
+  }
 }
