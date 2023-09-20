70ebbc9ab2f28d944e603d17cfe1e772ed2d63eb
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:28:14 +0200

    Update dashboard.module.ts

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './main/main.component';
 import { SharedModule } from '../shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, CalendarComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     SharedModule,
     HttpClientModule,
+    MenubarModule,
+    InputTextModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
 })
 export class DashboardModule {}
