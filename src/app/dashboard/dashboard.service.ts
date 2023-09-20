2a214908a67c6ada104a7b5498e3090245dac97b
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 22:55:09 +0200

    Update dashboard.service.ts

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Test } from '../interfaces/testInterface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +8,14 @@ import { Test } from '../interfaces/testInterface';
 export class DashboardService {
   constructor(private http: HttpClient) {}
 
-  getAllTests(page: number, size: number): Observable<any> {
+  getAllTests(page: number, size: number, filter: string): Observable<any> {
     return this.http.get(
-      'http://localhost:8080/api/v1/test?page=' + page + '&size=' + size
+      'http://localhost:8080/api/v1/test/' +
+        filter +
+        '?page=' +
+        page +
+        '&size=' +
+        size
     );
   }
 }
