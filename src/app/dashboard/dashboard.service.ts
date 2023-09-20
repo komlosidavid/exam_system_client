a6ad1169abc3b99f6886dd3e0863b3a58e2dfb04
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:28:12 +0200

    Update dashboard.service.ts

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -9,7 +9,9 @@ import { Test } from '../interfaces/testInterface';
 export class DashboardService {
   constructor(private http: HttpClient) {}
 
-  getAllTests(): Observable<Array<Test>> {
-    return this.http.get<Array<Test>>('http://localhost:8080/api/v1/test');
+  getAllTests(page: number, size: number): Observable<any> {
+    return this.http.get(
+      'http://localhost:8080/api/v1/test?page=' + page + '&size=' + size
+    );
   }
 }
