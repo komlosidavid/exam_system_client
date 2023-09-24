fa2def2f9301e06a0410fcc46d0d20e0bcdb54cf
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-24 14:33:23 +0200

    Update main.component.ts
    Changed filter and sort settings parameters.

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 import { Test } from 'src/app/interfaces/testInterface';
 import { MenuItem } from 'primeng/api';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -12,31 +13,47 @@ export class MainComponent implements OnInit {
   page: number = 0;
   size: number = 5;
   isLast: boolean = false;
-  tests!: Array<Test>;
+  tests!: Array<Test> | null;
+  filteredTests!: Array<Test>;
   menubarItems!: MenuItem[] | undefined;
   selectedFilter: string = 'All';
   isLoading: boolean = true;
+  isAllWasNull: boolean = true;
 
   constructor(private service: DashboardService) {}
 
   ngOnInit(): void {
+    var that = this;
     this.menubarItems = [
       {
         label: 'Filter',
+        icon: 'pi pi-filter',
+        disabled: false,
         items: [
           {
             label: 'All',
+            command(event) {
+              that.handleFilterChange(event.item?.label!);
+            },
           },
           {
             label: 'Own',
+            command(event) {
+              that.handleFilterChange(event.item?.label!);
+            },
           },
           {
             label: 'Collaborating',
+            command(event) {
+              that.handleFilterChange(event.item?.label!);
+            },
           },
         ],
       },
       {
         label: 'Sort',
+        icon: 'pi pi-sort',
+        disabled: false,
         items: [
           {
             label: 'By date',
@@ -44,25 +61,46 @@ export class MainComponent implements OnInit {
         ],
       },
     ];
-    this.loadData();
+    this.loadData(false);
+  }
+
+  private setFilterAndSortDisabled() {
+    if (!this.isAllWasNull) {
+      this.menubarItems?.forEach((item) => {
+        item.disabled = true;
+      });
+      this.tests = null;
+    }
   }
 
   loadMoreData(): void {
     if (!this.isLast) {
       this.page += 1;
-      this.loadData();
+      this.loadData(false);
     }
   }
 
-  private loadData(): void {
+  private handleFilterChange(filter: string) {
+    this.selectedFilter = filter;
+    this.loadData(true);
+  }
+
+  private loadData(isFilterChanged: boolean): void {
+    if (isFilterChanged) {
+      this.tests = null;
+    }
     this.service
       .getAllTests(this.page, this.size, this.selectedFilter.toUpperCase())
       .subscribe({
         next: (response) => {
-          if (!this.tests) {
-            this.tests = response.content;
+          if (response.content.length == 0) {
+            this.setFilterAndSortDisabled();
           } else {
-            this.tests.push(response.content);
+            if (!this.tests) {
+              this.tests = response.content;
+            } else {
+              this.tests.push(response.content);
+            }
           }
           if (response.last) {
             this.isLast = true;
