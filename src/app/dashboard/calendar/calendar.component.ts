56c58fc9d22d76377fd2060dab5409d653bffd6e
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-24 14:33:51 +0200

    Update calendar.component.ts

--- src/app/dashboard/calendar/calendar.component.ts
+++ src/app/dashboard/calendar/calendar.component.ts
@@ -1,26 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewChild,
-  TemplateRef,
-} from '@angular/core';
-import { EventColor } from 'calendar-utils';
-import { CalendarView, CalendarEvent } from 'angular-calendar';
-
-const colors: Record<string, EventColor> = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalendarView } from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar',
@@ -29,16 +8,6 @@ const colors: Record<string, EventColor> = {
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent {
-  // @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
-
-  // view: CalendarView = CalendarView.Month;
-
-  // CalendarView = CalendarView;
-
-  // viewDate: Date = new Date();
-
-  // modalData: {
-  //   action: string;
-  //   event: CalendarEvent;
-  // };
+  view: CalendarView = CalendarView.Month;
+  viewDate: Date = new Date();
 }
