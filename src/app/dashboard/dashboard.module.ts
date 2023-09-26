d788a06b62f668e4502002d1b4efafc1b581465f
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:29:14 +0200

    Update dashboard.module.ts
    First implementation of angular calendar.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainComponent } from './main/main.component';
@@ -9,9 +9,24 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SkeletonModule } from 'primeng/skeleton';
 import { CardModule } from 'primeng/card';
 import { CalendarComponent } from './calendar/calendar.component';
+import { CreateTestComponent } from './create-test/create-test.component';
+import { PanelModule } from 'primeng/panel';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AnswerComponent } from '../test/answer/answer.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MessagesModule } from 'primeng/messages';
 
 @NgModule({
-  declarations: [MainComponent, CalendarComponent],
+  declarations: [MainComponent, CalendarComponent, CreateTestComponent],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AnswerComponent),
+      multi: true,
+    },
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
@@ -21,6 +36,11 @@ import { CalendarComponent } from './calendar/calendar.component';
     InputTextModule,
     SkeletonModule,
     CardModule,
+    PanelModule,
+    SplitButtonModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    MessagesModule,
   ],
 })
 export class DashboardModule {}
