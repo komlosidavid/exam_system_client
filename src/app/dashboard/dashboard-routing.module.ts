9853bca09d6ff9c3c79eddb23a1ada3e15bc0abc
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:28:52 +0200

    Update dashboard-routing.module.ts
    Added route history.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { CreateTestComponent } from './create-test/create-test.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'create', component: CreateTestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 })
 export class DashboardRoutingModule {}
