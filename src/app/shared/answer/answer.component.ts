4772299255833709adcfc2203f824379c1e44216
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:26:37 +0200

    Create answer.component.ts

---
+++ src/app/shared/answer/answer.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-answer',
+  templateUrl: './answer.component.html',
+  styleUrls: ['./answer.component.css'],
+})
+export class AnswerComponent {
+  @Input()
+  answerGroup!: FormGroup;
+
+  @Output()
+  makeCorrect: EventEmitter<number> = new EventEmitter<number>();
+  @Output()
+  deleteAnswer: EventEmitter<number> = new EventEmitter<number>();
+  @Output()
+  onHandleMakeAnswerCorrectnessFalse: EventEmitter<void> =
+    new EventEmitter<void>();
+
+  toggleCorrectness(group: FormGroup): void {
+    if (group.get('type')?.value == 'one_answer') {
+      this.onHandleMakeAnswerCorrectnessFalse.emit();
+      const isCorrectControl = group.get('isCorrect');
+      isCorrectControl!.setValue(!isCorrectControl!.value);
+    } else {
+      const isCorrectControl = group.get('isCorrect');
+      isCorrectControl!.setValue(!isCorrectControl!.value);
+    }
+  }
+
+  onHandleDeleteAnswer(id: number): void {
+    this.deleteAnswer.emit(id);
+  }
+}
