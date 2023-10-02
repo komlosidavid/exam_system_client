4e8832828396ba6392ff5240dd9354da691a8305
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-02 23:07:15 +0200

    Update auth.effects.ts
    Implemented logout effect.

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -4,8 +4,6 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from 'src/app/auth/auth.service';
 import * as AuthActions from '../actions/auth.actions';
 import { catchError, exhaustMap, map, of, tap } from 'rxjs';
-import { AuthenticationResponse } from '../../interfaces/AuthenticationResponse';
-import { authReducer } from '../reducers/auth.reducers';
 
 @Injectable()
 export class AuthEffects {
@@ -45,56 +43,14 @@ export class AuthEffects {
     { dispatch: false }
   );
 
-  // LogIn: Observable<any> = createEffect(() =>
-  //   this.actions.pipe(
-  //     ofType(AuthActionTypes.LOGIN),
-  //     map((action: LogIn) => action.payload),
-  //     switchMap((payload) => {
-  //       return this.authService.onHandleAuthentication(payload).pipe(
-  //         map((user) => {
-  //           return new LogInSuccess({
-  //             token: user.accessToken,
-  //             username: payload.username,
-  //           });
-  //         }),
-  //         catchError((error) => {
-  //           return of(new LogInFailure({ error: error }));
-  //         })
-  //       );
-  //     })
-  //   )
-  // );
-
-  // LogInSuccess: Observable<any> = this.actions.pipe(
-  //   ofType(AuthActionTypes.LOGIN_SUCCESS),
-  //   tap((action) => {
-  //     console.log(action);
-
-  //     // localStorage.setItem('token', token);
-  //     // this.router.navigateByUrl('/dashboard');
-  //   })
-  // );
-
-  // LogInFailure: Observable<any> = createEffect(
-  //   () =>
-  //     this.actions.pipe(
-  //       ofType(AuthActionTypes.LOGIN_FAILURE)
-  //     ) as Observable<any>
-  // );
-
-  // LogOut: Observable<any> = createEffect(
-  //   () =>
-  //     this.actions.pipe(
-  //       ofType(AuthActionTypes.LOGOUT),
-  //       tap((user) => {
-  //         localStorage.removeItem('token');
-  //       })
-  //     ) as Observable<any>
-  // );
-
-  // TODO: Implement SignUp effect
-
-  // TODO: Implement SignUpSuccess effect
-
-  // TODO: Implement SignUpFailure effect
+  logOut$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap(() => {
+          this.router.navigateByUrl('/auth');
+        })
+      ),
+    { dispatch: false }
+  );
 }
