ab9d960f9ceca63fb72f9901ea91a0582332e059
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-02 23:06:46 +0200

    Delete userClass.model.ts

--- src/app/models/userClass.model.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.model';
-
-export class UserClass implements User {
-  fullname?: string | undefined;
-  id?: number | undefined;
-  password?: string | undefined;
-  token?: string | undefined;
-  username?: string | undefined;
-}
