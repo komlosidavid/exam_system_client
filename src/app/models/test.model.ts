60f4e354bcaad6f481de964af56bfb0794db4907
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-07 20:57:55 +0200

    Update test.model.ts

--- src/app/models/test.model.ts
+++ src/app/models/test.model.ts
@@ -5,6 +5,7 @@ import { User } from "./user.model";
 export interface Test {
     id?: number,
     subject: string,
+    opensAt: Date,
     creator: string,
     collaborators: Array<User>,
     students: Array<User>,
