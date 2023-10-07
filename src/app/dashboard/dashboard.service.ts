2164c31a30c0db4a362206b70f2787f5faada39b
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-07 20:57:39 +0200

    Update dashboard.service.ts
    Implemented search for user by name.

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, debounceTime, delay } from 'rxjs';
 import { Test } from '../models/test.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardService {
-  private URLTest: string = 'http://localhost:8080/api/v1/test';
+  private URLTest: string = 'http://localhost:8080/api/v1/test/';
   private URLUser: string = 'http://localhost:8080/api/v1/user/';
 
   constructor(private http: HttpClient) {}
 
   getAllTests(page: number, size: number, filter: string): Observable<any> {
     return this.http.get(
-      this.URLTest + '/' + filter + '?page=' + page + '&size=' + size
+      this.URLTest + filter + '?page=' + page + '&size=' + size
     );
   }
 
@@ -24,5 +25,9 @@ export class DashboardService {
 
   getAllUsersByRole(role: string): Observable<any> {
     return this.http.get(this.URLUser + 'get?role=' + role);
+  }
+
+  getAllUsersByContainingFullName(name: string) {
+    return this.http.get<Array<User>>(this.URLUser + 'get?fullname=' + name);
   }
 }
