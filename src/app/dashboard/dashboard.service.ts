9e6d64c516ff24296358ca4806fd4dd9f43c303c
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-09 22:10:06 +0200

    Update dashboard.service.ts
    Implemented get users method to get users by containing name and role.

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, debounceTime, delay } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Test } from '../models/test.model';
 import { User } from '../models/user.model';
 
@@ -8,14 +8,14 @@ import { User } from '../models/user.model';
   providedIn: 'root',
 })
 export class DashboardService {
-  private URLTest: string = 'http://localhost:8080/api/v1/test/';
+  private URLTest: string = 'http://localhost:8080/api/v1/test';
   private URLUser: string = 'http://localhost:8080/api/v1/user/';
 
   constructor(private http: HttpClient) {}
 
   getAllTests(page: number, size: number, filter: string): Observable<any> {
     return this.http.get(
-      this.URLTest + filter + '?page=' + page + '&size=' + size
+      this.URLTest + '/' + filter + '?page=' + page + '&size=' + size
     );
   }
 
@@ -27,7 +27,17 @@ export class DashboardService {
     return this.http.get(this.URLUser + 'get?role=' + role);
   }
 
-  getAllUsersByContainingFullName(name: string) {
-    return this.http.get<Array<User>>(this.URLUser + 'get?fullname=' + name);
+  getAllUsersByContainingNameAndRole(name: string, role: string) {
+    return this.http.get<Array<User>>(
+      this.URLUser + 'get?name=' + name + '&role=' + role
+    );
+  }
+
+  getAllTestsBySubjectName(subject: string) {
+    return this.http.get<Array<Test>>(this.URLTest + '/get?subject=' + subject);
+  }
+
+  getTestById(id: string): Observable<Test> {
+    return this.http.get<Test>(this.URLTest + '/test/' + id);
   }
 }
