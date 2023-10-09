a7e84d60a38c48df9085acd77b2febf1925bd307
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-09 22:07:30 +0200

    Update dashboard-routing.module.ts

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { CreateTestComponent } from './create-test/create-test.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { TestComponent } from '../test/test/test.component';
+import { EditTestComponent } from './edit-test/edit-test.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'create', component: CreateTestComponent },
+  { path: 'test/:id', component: TestComponent },
+  { path: 'edit/:id', component: EditTestComponent },
 ];
 
 @NgModule({
