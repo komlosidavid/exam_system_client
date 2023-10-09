fd612bd0ed0453a4532ea310ea2ffc40a20540be
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-09 22:11:14 +0200

    Update test.component.ts
    Implemented test component to handle test filling by user.

--- src/app/test/test/test.component.ts
+++ src/app/test/test/test.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Test } from 'src/app/models/test.model';
+import { TestService } from '../test.service';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-test',
@@ -9,13 +17,51 @@ import { Test } from 'src/app/models/test.model';
 })
 export class TestComponent implements OnInit {
   test!: Test;
+  form!: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: TestService,
+    private _fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
-      this.test = JSON.parse(params['data']);
-      console.log(this.test);
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.service.getTestById(id!).subscribe({
+      next: (data) => {
+        this.test = data;
+
+        this.form = this._fb.group({
+          subject: this.test.subject,
+          questions: this._fb.array([]),
+        });
+
+        this.test.questions.forEach((question) => {
+          const answersTemp: FormArray = this._fb.array([]);
+          question.answers.forEach((answer) => {
+            answersTemp.push(
+              this._fb.group({
+                type: answer.type.toLowerCase(),
+                answerSelect: new FormControl({ value: '', disabled: false }),
+                answer: answer.answer,
+              })
+            );
+          });
+
+          this.questions.push(
+            this._fb.group({
+              type: question.type.toLowerCase(),
+              question: question.question,
+              points: question.points,
+              answers: answersTemp,
+            })
+          );
+        });
+      },
     });
+  }
+
+  get questions(): FormArray {
+    return this.form.get('questions') as FormArray;
   }
 }
